55decbbde560853367fc4cfd015c48210c01dd90
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-12 23:53:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(times)
   return "HAPPY NEW YEAR!"
 end
 
+#write your code here
+
+def countdown_with_sleep(times)
+  while (times > 0)
+  puts "#{times} SECOND(S)!"
+    times-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
